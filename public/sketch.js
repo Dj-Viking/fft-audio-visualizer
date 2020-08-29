7941d41e124b0639a99383d1239e4c5013c66987
DJ Viking <anders.swedishviking@gmail.com>
2020-08-29 17:04:12 -0400

    made userSTartAudio invoke on webpage load

--- public/sketch.js
+++ public/sketch.js
@@ -1,20 +1,41 @@
 let mic;
+let fft;
 let vol;
 const micOff = document.querySelector('#btn-off');
 const micOn = document.querySelector('#btn-on');
+
+let audioHistory = [];
 
 function setup() {
   createCanvas(400,400);
   mic = new p5.AudioIn();
   mic.start();
+  fft = new p5.FFT();
+  userStartAudio();
 }
 
 function draw() {
   background(0);
   vol = mic.getLevel();
-  ellipse(200, 200, 400, vol*800);
-  
-  console.log(vol);
+  let spectrum = fft.analyze();
+  //console.log(spectrum);
+  stroke(0, 255, 0);
+  fill(0, 255, 0);
+  ellipse(200, 200, vol*400, vol*800);
+  audioHistory.push(vol);
+  //console.log(audioHistory);
+  //console.log(vol);
+  stroke(0, 255, 0);
+  beginShape();
+  for (let i = 0; i < audioHistory.length; i++) {
+    let y = map(audioHistory[i], 0, 1, height/2, 0);
+    vertex(i, y);
+  }
+  endShape();
+
+  if(audioHistory.length > width) {
+    audioHistory.splice(0, 1);
+  }
 }
 
 micOn.addEventListener('click', micStart);
